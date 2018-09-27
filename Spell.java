7b5a7b423ef8783f75f468a9ece01496f4e3f818
Adit <harhar.aditya@gmail.com>
2018-09-27 09:22:14 -0700

    Initial sharing of project

---
+++ Spell.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Spell
+{
+    // instance variables - replace the example below with your own
+    String name; 
+    double attackRating,damageRating,trainingTime;
+    
+    public Spell()
+    {
+        // initialise instance variables
+    }
+}
---
+++ Wizard.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Wizard
+{
+    String name;
+    double health;
+    ArrayList<Spell> spellBook = new ArrayList<Spell>(); 
+    public Wizard(String n,double h)
+    {
+        // initialise instance variables
+        name = n;
+        health = h;
+    }
+}
